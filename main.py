a78d7efe4a0378c377f9eecd07e1e6049ff62558
JeanSkripchenko <jeanskrip1407@gmail.com>
2021-12-12 21:29:42 +0200

    Fix main get_prefix

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ config = Config()
 async def init_and_run_bot(token: str) -> None:
     bot = Leyla(
         owner_ids=[848593011038224405, 880028714841305150, 598387707311554570],
-        command_prefix=config.get_prefix(),
+        command_prefix=config.get_prefix,
         allowed_mentions=disnake.AllowedMentions(
             everyone=False,
             replies=True,
