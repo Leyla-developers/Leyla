15b1f4153d3b8513b2901340102a499e01f3ef71
JeanSkripchenko <jeanskrip1407@gmail.com>
2021-12-12 22:20:06 +0200

    Fix load_cogs

--- core/bot.py
+++ core/bot.py
@@ -1,3 +1,4 @@
+from disnake import integrations
 import yaml
 from datetime import datetime
 from typing import Iterable
@@ -14,7 +15,6 @@ class Leyla(commands.Bot):
         super().__init__(**kwargs)
         self.config = kwargs.get('config')
         self.uptime = datetime.utcnow()
-        self.load_cogs(['cogs.slash_commands', 'cogs.message_commands', 'cogs.events'])
 
     def __getitem__(self, item: str) -> commands.Command:
         return self.get_command(item)
@@ -25,7 +25,7 @@ class Leyla(commands.Bot):
     def load_cogs(self, folders: Iterable, ignore_cogs: Iterable=None):
         for folder in folders:
             for cog in find_extensions_in(folder):
-                if ignore_cogs is not None and not cog in ignore_cogs:
+                if ignore_cogs is None or ignore_cogs is not None and not cog in ignore_cogs:
                     try:
                         self.load_extension(cog)
                     except Exception as e:
@@ -37,6 +37,9 @@ class Leyla(commands.Bot):
 
     async def get_context(self, message, *, cls=Context):
         return await super().get_context(message=message, cls=cls)
+
+    async def on_connect(self):
+        self.load_cogs(['cogs.slash_commands', 'cogs.message_commands', 'cogs.events'])
 
     async def on_ready(self):
         print(self.user.name, 'started at:', str(self.uptime))
