2de13e8fe37346b75fefa06ce9249761f0009062
JeanSkripchenko <jeanskrip1407@gmail.com>
2021-12-12 21:52:37 +0200

    Fix config

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 from os import environ
+from typing import Union
 
 from disnake import Guild
 from disnake.ext import commands
@@ -9,7 +10,7 @@ class Config:
 
     MONGO_CLIENT = AsyncIOMotorClient(environ['DB']) 
     DB = MONGO_CLIENT.Leyla
-    DEFAULT_GUILD_DATA = {'prefix': 'l.', 'color': ...}
+    DEFAULT_GUILD_DATA = {'prefix': 'l.', 'color': 0xa8a6f0}
 
     def __init__(self) -> None:
         pass
@@ -19,9 +20,10 @@ class Config:
             prefix = await self.get_guild_data(message.guild.id, key='prefix')
             return commands.when_mentioned_or(*[prefix.lower(), prefix.upper()])(bot, message)
 
-    async def get_guild_data(self, guild: commands.GuildConverter, key: str=None) -> dict:
-        if await self.DB.guilds.count_documents({"_id": guild.id}) != 0:
-            data = await self.DB.guilds.find_one({"_id": guild.id})
+    async def get_guild_data(self, guild: Union[Guild, int], key: str=None) -> dict:
+        guild_id = guild.id if isinstance(guild, Guild) else guild
+        if await self.DB.guilds.count_documents({"_id": guild_id}) != 0:
+            data = await self.DB.guilds.find_one({"_id": guild_id})
         else:
             data = self.DEFAULT_GUILD_DATA
         return data.get(key) if key is not None else data
