fae03575f847abba0f988ee6a8f0c6e516457aef
JeanSkripchenko <jeanskrip1407@gmail.com>
2021-12-25 19:50:11 +0200

    да бля я ебал

--- services/waifu_pics.py
+++ services/waifu_pics.py
@@ -7,6 +7,6 @@ BASE_URL = 'https://api.waifu.pics'
 
 
 async def get_image(type: str, category: str, session = aiohttp.ClientSession()):
-    async with session.get(f'/{BASE_URL}/{type}/{category}') as response:
+    async with session.get(f'{BASE_URL}/{type}/{category}') as response:
         data = await response.json()
         return data.get('url')
