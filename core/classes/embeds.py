29cb8a46c853ae7da32b4680c6640258eae5bbc3
MagM1go <68418026+MagM1go@users.noreply.github.com>
2021-12-26 11:32:44 +0800

    Update embeds.py

--- core/classes/embeds.py
+++ core/classes/embeds.py
@@ -10,20 +10,14 @@ class Embeds(Embed):
     def __init__(self, default_color) -> None:
         self.default_color = default_color
 
-    async def simple(self, ctx: Union[Context, disnake.ApplicationCommandInteraction]=None, title: str=None, description: str=None, image: str=None, thumbnail: str=None, **kwargs):
-        embed = self.from_dict(**kwargs)
+    async def simple(self, ctx: Union[Context, disnake.ApplicationCommandInteraction]=None, image: str=None, thumbnail: str=None, **kwargs):
+        embed = Embed(**kwargs)
 
         if isinstance(embed.color, self.Empty):
             embed.color = self.default_color if not ctx else ctx.config.get_guild_data(ctx.guild.id, key='color')
 
         if ctx:
             embed.set_author(name=ctx.author.display_name, icon_url=ctx.author.display_avatar.url)
-        
-        if title:
-            embed.title = title
-    
-        if description:
-            embed.description = description
 
         if image:
             embed.set_image(url=image)
