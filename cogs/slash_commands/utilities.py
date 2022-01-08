39a65bdfeb8221f13fb2c4b104618287f04c5a57
MagM1go <68418026+MagM1go@users.noreply.github.com>
2022-01-09 00:30:56 +0800

    Ну же, давай!

--- cogs/slash_commands/utilities.py
+++ cogs/slash_commands/utilities.py
@@ -101,7 +101,7 @@ class Utilities(commands.Cog):
         await interaction.send(
             embed=await self.bot.embeds.simple(
                 title=f"Эмодзи **{emoji}**", 
-                image=self.bot.get_emoji(int("".join(re.findall(r'[0-9]', emoji)))).url
+                image=f"https://cdn.discordapp.com/emojis/{int(''.join(re.findall(r'[0-9]', emoji)))}.webp?size=44&quality=lossless"
                 )
             )
 
