9783da9aa2aad76e80bf7fd5c171ec575649dc4e
MagM1go <68418026+MagM1go@users.noreply.github.com>
2022-01-09 00:10:10 +0800

    что и где ему не нравится

--- cogs/slash_commands/utilities.py
+++ cogs/slash_commands/utilities.py
@@ -21,18 +21,18 @@ class Utilities(commands.Cog):
     @commands.slash_command(
         description="Вывод аватара участника"
     )
-    async def avatar(self, ctx: commands.Context, user: disnake.User=None):
-        user = user if user else ctx.author
+    async def avatar(self, interaction: commands.Context, user: disnake.User=None):
+        user = user if user else interaction.author
         embed = await self.embed.simple(
             title=f"Аватар {'бота' if user.bot else 'пользователя'} {user.name}",
             image=user.display_avatar.url
         )
-        return await ctx.send(embed=embed)
+        return await interaction.send(embed=embed)
 
     @commands.slash_command(
         description='Перевод в/из азбуки морзе.'
     )
-    async def morse(self, ctx: commands.Context, variant: typing.Literal['to', 'from'], *, code):
+    async def morse(self, interaction: commands.Context, variant: typing.Literal['to', 'from'], *, code):
         if variant == 'to':
             morse = Decoder().to_morse(code)
         elif variant == 'from':
@@ -42,34 +42,34 @@ class Utilities(commands.Cog):
             title='Decoder/Encoder морзе.',
             description=morse
         )
-        await ctx.send(embed=embed)
+        await interaction.send(embed=embed)
 
     @commands.slash_command(
         description="Вывод информации о гильдии",
     )
-    async def guild(self, ctx):
+    async def guild(self, interaction):
         information = (
-            f'Участников: **{len(ctx.guild.members)}**',
-            f'Эмодзи: **{len(ctx.guild.emojis)}**',
-            f'Ролей: **{len(ctx.guild.roles)}**',
+            f'Участников: **{len(interaction.guild.members)}**',
+            f'Эмодзи: **{len(interaction.guild.emojis)}**',
+            f'Ролей: **{len(interaction.guild.roles)}**',
         )
         embed = await self.bot.embeds.simple(
-            title=f'Информация о гильдии {ctx.guild.name}',
+            title=f'Информация о гильдии {interaction.guild.name}',
             description="\n".join(information)
         )
 
-        if ctx.guild.banner:
-            embed.set_thumbnail(ctx.guild.banner.url)
+        if interaction.guild.banner:
+            embed.set_thumbnail(interaction.guild.banner.url)
 
-        if ctx.guild.icon:
-            embed.set_thumbnail(ctx.guild.icon)
+        if interaction.guild.icon:
+            embed.set_thumbnail(interaction.guild.icon)
 
-        await ctx.send(embed=embed)
+        await interaction.send(embed=embed)
 
     @commands.slash_command(
         description="Вывод информации о юзере"
     )
-    async def user(self, ctx, user: disnake.User = commands.Param(lambda ctx: ctx.author)):
+    async def user(self, interaction, user: disnake.User = commands.Param(lambda interaction: interaction.author)):
         embed = await self.bot.embeds.simple(title=f'Информация о {"боте" if user.bot else "пользователе"} {user.name}')
 
         if user.banner:
@@ -83,22 +83,22 @@ class Utilities(commands.Cog):
             f"Полный никнейм: **{str(user)}**",
         ]
 
-        if user in ctx.guild.members:
-            user_to_member = ctx.guild.get_member(user.id)
+        if user in interaction.guild.members:
+            user_to_member = interaction.guild.get_member(user.id)
             second_information = [
                 f"Зашёл(-ла) на сервер: **<t:{round(user_to_member.joined_at.timestamp())}:R> | {(datetime.utcnow() - user.created_at.replace(tzinfo=None)).days} дней**",
                 f"Количество ролей: **{len(list(filter(lambda role: role, user_to_member.roles)))}**",
             ]
 
-        embed.description = "\n".join(main_information) + "\n" + "\n".join(second_information) if user in ctx.guild.members else "\n".join(main_information)
+        embed.description = "\n".join(main_information) + "\n" + "\n".join(second_information) if user in interaction.guild.members else "\n".join(main_information)
 
-        await ctx.send(embed=embed)
+        await interaction.send(embed=embed)
 
     @commands.slash_command(
         description="Получить эмодзик"
     )
-    async def emoji(self, ctx, emoji):
-        await ctx.send(embed=await self.bot.embeds.simple(image=self.bot.get_emoji(re.findall(r'[0-9]', emoji))))
+    async def emoji(self, interaction, emoji):
+        await interaction.send(embed=await self.bot.embeds.simple(image=self.bot.get_emoji(re.findall(r'[0-9]', emoji))))
 
 
 def setup(bot: commands.Bot):
