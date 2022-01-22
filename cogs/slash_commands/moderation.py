b95ddf5f30836131f53b8ba4b545e3418d096fb6
MagM1go <68418026+MagM1go@users.noreply.github.com>
2022-01-22 10:53:24 +0800

    Update moderation.py

--- cogs/slash_commands/moderation.py
+++ cogs/slash_commands/moderation.py
@@ -9,17 +9,29 @@ class Moderation(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    async def role_check(ctx, member):
+        if ctx.author.top_role.position <= member.top_role.position:
+            return False
+        
+        else:
+            return True
+
     @commands.slash_command(
         description="Можете теперь спокойно выдавать предупреждения uwu."
     )
     @commands.has_permissions(ban_members=True)
     async def warn(self, ctx, member: disnake.Member, *, reason: str = None):
         warn_id = random.randint(10000, 99999)
-        embed = await self.bot.embeds.simple(description=f"**{member.name}** было выдано предупреждение")
-        embed.add_field(name="Причина", value=reason)
-        embed.set_footer(text=f"ID: {warn_id}")
+        embed = await self.bot.embeds.simple(thumbnail=ctx.author.display_avatar.url)
+        embed.set_footer(text=f"ID: {warn_id} | {reason}")
+        
+        if await self.role_check(ctx):
+            embed.description = f"**{member.name}** было выдано предупреждение"
+            await self.bot.config.DB.moderation.insert_one({"_id": ctx.guild.id, "member": member.id, "reason": reason if reason else "Нет причины", "warn_id": warn_id})
+        
+        else:
+            embed.description = "У вас недостаточно прав."
 
-        await self.bot.config.DB.moderation.insert_one({"_id": ctx.guild.id, "member": member.id, "reason": reason if reason else "Нет причины", "warn_id": warn_id})
         await ctx.send(embed=embed)
 
 def setup(bot):
