515624247a809f3f9960f9c3dd85a6342fef408a
MagM1go <68418026+MagM1go@users.noreply.github.com>
2022-01-28 20:12:09 +0800

    ктоя

--- cogs/slash_commands/fun.py
+++ cogs/slash_commands/fun.py
@@ -1,7 +1,9 @@
 from random import randint
 
 import disnake
-from disnake.ext import commands, slash_commands
+from disnake.ext import commands
+
+from services import waifu_pics
 
 
 class FunSlashCommands(commands.Cog):
@@ -9,6 +11,51 @@ class FunSlashCommands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.slash_command(description='Случайное число в заданном диапазоне', )
+    @commands.slash_command(
+        guild_ids=[885541278908043304],
+        options=[
+            disnake.Option(name='a', type=disnake.OptionType.integer, description='Число от:'),
+            disnake.Option(name='b', type=disnake.OptionType.integer, description='Число до:')
+        ],
+        description='Случайное число в заданном диапазоне'
+    )
     async def number(self, inter: disnake.ApplicationCommandInteraction, a: int, b: int):
-        return await inter.send(randint(a, b))
+        embed = self.bot.embeds(title=f'Случайное число от `{a}` до `{b}`', thumbnail=inter.author.avatar.url)
+        embed.add_field(name='Ваше число...', value=randint(a, b))
+        return await inter.send(embed=embed)
+
+    @commands.slash_command(
+        guild_ids=[885541278908043304],
+        options=[
+            disnake.Option(
+                'choice', 'выберите наложение', 
+                type=disnake.OptionType.string,
+                required=True, 
+                choices=['wasted', 'jail', 'comrade', 'gay', 'glass', 'passed', 'triggered']
+            ),
+        ],
+        name='avatar-overlay'
+    )
+    async def jail_image(self, inter: disnake.ApplicationCommandInteraction, overlay: str, user: disnake.User = commands.Param(lambda inter: inter.author)):
+        embed = self.bot.embeds(title=f'`{user}` За шо сидит?', image=f'https://some-random-api.ml/canvas/{overlay}?avatar={str(user.avatar)}')
+        return await inter.send(embed=embed)
+
+    @commands.slash_command(
+        guild_ids=[885541278908043304],
+        options=[
+            disnake.Option(
+                'choice', 'Выберите тянку OwO', 
+                type=disnake.OptionType.string,
+                required=True, 
+                choices=[disnake.OptionChoice('megumin'), disnake.OptionChoice('shinobu'), disnake.OptionChoice('awoo')]
+            )
+        ],
+        name='anime-girl'
+    )
+    async def anime_girl(self, inter: disnake.ApplicationCommandInteraction, choice: str):
+        embed = self.bot.embeds(title=f'{choice.title()} OwO', image=await waifu_pics.get_image('sfw', choice.lower()))
+        return await inter.send(embed=embed)
+
+
+def setup(bot):
+    bot.add_cog(FunSlashCommands(bot))
--- cogs/slash_commands/rp.py
+++ cogs/slash_commands/rp.py
@@ -16,6 +16,29 @@ RP_DESCRIPTIONS = {
     'bite': 'Сделал(-а) кусь {user}',
 }
 
+RP_DESCRIPTIONS_MYSELF = {
+    'pat': 'Погладил(-а) себя',
+    'hug': 'Обнял(-а) себя',
+    'kiss': 'Поцеловал(-а) себя',
+    'lick': 'Облизнул(-а) себя',
+    'cuddle': 'Прижал(-а) себя к себе',
+    'handhold': 'Взял(-а) себя за руку',
+    'nom': 'Покормил(-а) себя',
+    'slap': 'Дал(-а) себе пощёчину',
+    'bite': 'Укусил(-а) себя'
+}
+
+RP_DESCRIPTIONS_LEYLA = {
+    'pat': 'Погладил(-а) {user}',
+    'hug': 'Обнял(-а) {user}',
+    'kiss': 'Поцеловал(-а) {user}',
+    'lick': 'Облизнул(-а) {user}',
+    'cuddle': 'Прижал(-а) к себе {user}',
+    'handhold': 'Взял(-а) за руку {user}',
+    'nom': 'Покормил(-а) {user}',
+    'slap': 'Дал(-а) пощечину {user}',
+    'bite': 'Ай... За шо? qwq',
+}
 
 class RolePlay(commands.Cog):
 
@@ -37,9 +60,10 @@ class RolePlay(commands.Cog):
         ]
     )
     async def rp(self, inter: disnake.ApplicationCommandInteraction, user: disnake.User = commands.Param(lambda inter: inter.author), choice: str=None):
+        descriptions = RP_DESCRIPTIONS if user != inter.author and user != self.bot.user else RP_DESCRIPTIONS_MYSELF if user == inter.author else RP_DESCRIPTIONS_LEYLA
         embed = await self.bot.embeds.simple(
             inter, 
-            description=f'***{RP_DESCRIPTIONS[choice].format(user=user)}***',
+            description=f'***{descriptions[choice].format(user=user)}***',
             image=await waifu_pics.get_image('sfw', choice)
         )
         return await inter.send(embed=embed)
