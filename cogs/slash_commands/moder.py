4aaf69128b475916286ba1a947a90f9bda38991a
MagM1go <68418026+MagM1go@users.noreply.github.com>
2022-02-05 18:20:13 +0800

    Update moder.py

--- cogs/slash_commands/moder.py
+++ cogs/slash_commands/moder.py
@@ -27,7 +27,7 @@ class Moderation(commands.Cog):
         
         if await self.checker(ctx, member):
             embed.description = f"**{member.name}** было выдано предупреждение"
-            await self.bot.config.DB.moderation.insert_one({"guild": ctx.guild.id, "member": member.id, "reason": reason if reason else "Нет причины", "warn_id": warn_id})
+            await self.bot.config.DB.warns.insert_one({"guild": ctx.guild.id, "member": member.id, "reason": reason if reason else "Нет причины", "warn_id": warn_id})
         else:
             if ctx.author.top_role.position <= member.top_role.position:
                 raise CustomError("Ваша роль равна или меньше роли упомянутого участника.")
@@ -43,10 +43,10 @@ class Moderation(commands.Cog):
         if member.bot:
             raise CustomError("Невозможно просмотреть предупреждения **бота**")
         else:
-            if await self.bot.config.DB.moderation.count_documents({"guild": ctx.guild.id, "member": member.id}) == 0:
+            if await self.bot.config.DB.warns.count_documents({"guild": ctx.guild.id, "member": member.id}) == 0:
                 raise CustomError("У вас/участника отсутствуют предупреждения.")
             else:
-                warn_description = "\n".join([f"{i['reason']} | {i['warn_id']}" async for i in self.bot.config.DB.moderation.find({"guild": ctx.guild.id})])
+                warn_description = "\n".join([f"{i['reason']} | {i['warn_id']}" async for i in self.bot.config.DB.warns.find({"guild": ctx.guild.id})])
     
                 embed = await self.bot.embeds.simple(
                     title=f"Вилкой в глаз или... Предупреждения {member.name}", 
@@ -64,15 +64,17 @@ class Moderation(commands.Cog):
     )
     async def unwarn(self, ctx, member: disnake.Member, warn_id: int):
         if self.checker(ctx, member):
-            await self.bot.config.DB.moderation.delete_one({"guild": ctx.guild.id, "member": member.id, "warn_id": warn_id})
+            await self.bot.config.DB.warns.delete_one({"guild": ctx.guild.id, "member": member.id, "warn_id": warn_id})
             await ctx.send(embed=await self.bot.embeds.simple(
                 title=f"Снятие предупреждения с {member.name}", 
                 description="Предупреждение участника было снято! :з", 
                 footer={"text": f"Модератор: {member.name}", "icon_url": member.display_avatar.url}
             )
         )
-        elif await self.bot.config.DB.moderation.count_documents({"guild": ctx.guild.id, "member": member.id}) == 0:
+        elif await self.bot.config.DB.warns.count_documents({"guild": ctx.guild.id, "member": member.id}) == 0:
             raise CustomError("У этого чудика нет предупреждений(")
+        elif await self.bot.config.DB.warns.count_documents({"guild": ctx.guild.id, "warn_id": warn_id}) == 0:
+            raise CustomError("")
         else:
             raise CustomError("Вы не можете снять предупреждение с себя.")
 
