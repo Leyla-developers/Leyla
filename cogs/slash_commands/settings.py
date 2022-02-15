c5d8c04d8de5b4f75f8c0c6bddb86a9541ae47a7
MagM1go <68418026+MagM1go@users.noreply.github.com>
2022-02-15 22:34:43 +0800

    Update settings.py

--- cogs/slash_commands/settings.py
+++ cogs/slash_commands/settings.py
@@ -24,30 +24,6 @@ class Settings(commands.Cog):
     async def levels(self, inter):
         ...
 
-    @automoderation.sub_command(description="Настройка наказания для любителей покричать (Caps Lock)")
-    async def capslock(self, inter, action: Literal['ban', 'timeout', 'kick', 'warn'], percent: int = 50, message: str = None):
-        if await self.bot.config.DB.automod.count_documents({"_id": inter.guild.id}) == 0:
-            await self.bot.config.DB.automod.insert_one({"_id": inter.guild.id, "action": action, "percent": percent, "message": message})
-        else:
-            if action == "timeout": 
-                data = {
-                    "timeout": {
-                        "duration": 43200
-                    }
-                }
-                await self.bot.config.DB.automod.update_one({"_id": inter.guild.id}, {"$set": {"action": data, "message": message}})
-            else:
-                await self.bot.config.DB.automod.update_one({"_id": inter.guild.id}, {"$set": {"action": action, "message": message}})
-
-        await inter.send(
-            embed=await self.bot.embeds.simple(
-                title='Leyla settings **(automoderation)**', 
-                description=f"Установлено наказание в случае превышения капса",
-                footer={"text": f"Наказание: {action}", "icon_url": inter.guild.icon.url if inter.guild.icon.url else None}
-            )
-        )
-
-
     @levels.sub_command(description="Настройка системы уровней")
     async def mode(self, inter, _mode: Literal['Включить', 'Выключить']):
         mode = {
