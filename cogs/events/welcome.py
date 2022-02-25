3aad9268852972e5766cc3708262c29e461c5779
MagMigo <magmigo2@inbox.ru>
2022-02-25 17:08:43 +0800

    упс.

--- cogs/events/welcome.py
+++ cogs/events/welcome.py
@@ -12,16 +12,17 @@ class Welcome(commands.Cog):
         if await self.bot.config.DB.welcome.count_documents({"_id": member.guild.id}) == 0:
             return
         else:
-            if dict(await self.bot.config.DB.welcome.find_one({"_id": message.guild.id}))['message']:
-                data = dict(await self.bot.config.DB.welcome.find_one({"_id": message.guild.id}))
-                message = {
-                    "[memberMention]": member.mention,
-                    '[member]': member.name,
-                    '[guild]': member.guild.name,
-                    '[guildMembers]': len(member.guild.members),
-                }
+            if dict(await self.bot.config.DB.welcome.find_one({"_id": member.guild.id}))['channel']:
+                if dict(await self.bot.config.DB.welcome.find_one({"_id": member.guild.id}))['message']:
+                    data = dict(await self.bot.config.DB.welcome.find_one({"_id": member.guild.id}))
+                    message = {
+                        "[memberMention]": member.mention,
+                        '[member]': member.name,
+                        '[guild]': member.guild.name,
+                        '[guildMembers]': len(member.guild.members),
+                    }
 
-                await member.guild.get_channel(data['channel']).send(message[data['message']])
+                    await member.guild.get_channel(data['channel']).send(message[data['message']])
 
 def setup(bot):
     bot.add_cog(Welcome(bot))
