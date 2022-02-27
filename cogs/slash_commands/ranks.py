1fff0b2d53960ee03dd51301ab26ad654032ba8a
MagMigo <magmigo2@inbox.ru>
2022-02-27 17:05:40 +0800

    Update ranks.py

--- cogs/slash_commands/ranks.py
+++ cogs/slash_commands/ranks.py
@@ -20,6 +20,19 @@ class Ranks(commands.Cog):
         else:
             return False
 
+    async def get_level_up_message(self, message):
+        if dict(await self.bot.config.DB.levels.find_one({"guild": message.guild.id}))['message']:
+            data = dict(await self.bot.config.DB.levels.find_one({"guild": message.guild.id, "member": message.author.id}))
+            channel_id = dict(await self.bot.config.DB.levels.find_one({"_id": message.guild.id}))['channel']
+            data = dict(await self.bot.config.DB.levels.find_one({"guild": message.guild.id}))['message']
+            data = data.replace("[xp]", data['xp'])
+            data = data.replace("[lvl]", data['lvl'])
+            data = data.replace("[member]", message.author.name)
+            data = data.replace("[memberMention]", message.author.mention)
+            data = data.replace("[channel]", message.channel.mention)
+
+            return await message.guild.get_channel(channel_id).send(data)
+
     @commands.Cog.listener()
     async def on_message(self, message: disnake.Message):
         if await self.bot.config.DB.levels.count_documents({"_id": message.guild.id}) == 0:
@@ -30,26 +43,21 @@ class Ranks(commands.Cog):
 
         else:
             if dict(await self.bot.config.DB.levels.find_one({"_id": message.guild.id}))['mode']:
-                data = dict(await self.bot.config.DB.levels.find_one({"guild": message.guild.id, "member": message.author.id}))
-                channel_id = dict(await self.bot.config.DB.levels.find_one({"_id": message.guild.id}))['channel']
-                data = dict(await self.bot.config.DB.levels.find_one({"guild": message.guild.id}))['message']
-                data = data.replace("[xp]", data['xp'])
-                data = data.replace("[lvl]", data['lvl'])
-                data = data.replace("[member]", message.author.name)
-                data = data.replace("[memberMention]", message.author.mention)
-                data = data.replace("[channel]", message.channel.mention)
-
                 if await self.formula(message, message.author):
-                    lvl = dict(await self.bot.config.DB.levels.find_one({"guild": message.guild.id, "member": message.author.id}))['lvl']
-                    await self.bot.config.DB.levels.update_one({"guild": message.guild.id, "member": message.author.id}, {"$set": {"xp": 0, "lvl": lvl + 1}})
-                    await message.guild.get_channel(channel_id).send(data)
+                    if await self.get_level_up_message(message):
+                        await self.get_level_up_message(message)
+
                     if dict(await self.bot.config.DB.levels.find_one({"_id": message.guild.id}))['roles']:
                         level_role_data = dict(await self.bot.config.DB.levels.find_one({"_id": message.guild.id}))['roles']
                         reverse_levels = {value: key for key, value in level_role_data.items()}
                         await message.author.add_roles(message.guild.get_role(int(reverse_levels[[i for i in level_role_data.values() if lvl >= int(i)][0]])))
-                    else:
-                        await sleep(5)
-                        await self.bot.config.DB.levels.update_one({"guild": message.guild.id, "member": message.author.id}, {"$set": {"xp": __import__('random').randint(2, 5)+data['xp']}})
+
+                    lvl = dict(await self.bot.config.DB.levels.find_one({"guild": message.guild.id, "member": message.author.id}))['lvl']
+                    await self.bot.config.DB.levels.update_one({"guild": message.guild.id, "member": message.author.id}, {"$set": {"xp": 0, "lvl": lvl + 1}})
+
+                else:
+                    await sleep(5)
+                    await self.bot.config.DB.levels.update_one({"guild": message.guild.id, "member": message.author.id}, {"$set": {"xp": __import__('random').randint(2, 5)+data['xp']}})
 
     @commands.slash_command(description="Узнать свой (или пользователя) опыт/уровень")
     async def rank(self, inter, member: disnake.Member = commands.Param(lambda inter: inter.author)):
