a9d998dff71c8ac5b534505dde10b9bec86909e8
MagMigo <magmigo2@inbox.ru>
2022-03-07 22:43:31 +0800

    Update emoji_role.py

--- cogs/events/emoji_role.py
+++ cogs/events/emoji_role.py
@@ -15,6 +15,7 @@ class EmojiRole(commands.Cog):
 
     @commands.Cog.listener()
     async def on_raw_reaction_add(self, payload: disnake.RawReactionActionEvent):
+        if payload.member.bot: return
         if await self.get_data_from_db(payload.message_id):
             data = dict(await self.get_data_from_db(payload.message_id))
 
@@ -25,12 +26,13 @@ class EmojiRole(commands.Cog):
 
     @commands.Cog.listener()
     async def on_raw_reaction_remove(self, payload: disnake.RawReactionActionEvent):
+        if payload.member.bot: return
         if await self.get_data_from_db(payload.message_id):
             data = dict(await self.get_data_from_db(payload.message_id))
 
             if data['_id'] == payload.message_id:
                 if payload.emoji in data['emoji'].keys():
-                    for i in data['emoji']['roles']: # {"emoji": {"here_emoji": [roles]}}
+                    for i in data['emoji']['roles']: # {"emoji": {"here_emoji": ['123456789012345678', ...]}}
                         await payload.member.add_roles(self.bot.get_guild(payload.guild_id).get_role(int(i)))
 
 
