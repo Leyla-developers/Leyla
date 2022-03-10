992c18f2ea3020332ee1df273df1022cd3849961
MagMigo <magmigo2@inbox.ru>
2022-03-10 20:48:31 +0800

    Update buttons.py

--- Tools/buttons.py
+++ Tools/buttons.py
@@ -44,18 +44,5 @@ class CurrencyButton(disnake.ui.View):
 
     def __init__(self):
         super().__init__()
-    
-    @disnake.ui.button(
-        label="Остальные существующие валюты", 
-        style=disnake.ButtonStyle.blurple,
-    )
-    async def currency(self, button, inter):
-        cur_data = "\n".join(['AUD - Австралийский доллар', 'AZN - Азербайджанский манат', 'GBP - Фунт стерлингов Соединенного королевства', 'AMD - Армянских драмов', 'BYN - Белорусский рубль', 'BGN - Болгарский лев', 'BRL - Бразильский реал', 'HUF - Венгерских форинтов', 'HKD - Гонконгский доллар', 'DKK - Датская крона', 'USD - Доллар США', 'EUR - Евро', 'INR - Индийских рупий', 'KZT - Казахстанских тенге', 'CAD - Канадский доллар', 'KGS - Киргизских сомов', 'CNY - Китайский юань', 'MDL - Молдавских леев', 'NOK - Норвежская крона', 'PLN - Польский злотый', 'RON - Румынский лей', 'XDR - СДР (специальные права заимствования)', 'SGD - Сингапурский доллар', 'TJS - Таджикских сомони', 'TRY - Турецких лир', 'TMT - Новый туркменский манат', 'UZS - Узбекских сумов', 'UAH - Украинских гривен', 'CZK - Чешских крон', 'SEK - Шведская крона', 'CHF - Швейцарский франк', 'ZAR - Южноафриканских рэндов', 'KRW - Вон Республики Корея', 'JPY - Японских иен'])
-        
-        async with aiohttp.ClientSession().post(
-            'https://www.toptal.com/developers/hastebin/documents', 
-            data=cur_data
-        ) as response:
-            pastebin_data = await response.json()
 
-        await inter.response.send_message(f"Ваша ссылка: [нажмите, чтобы просмотреть все валюты](https://www.toptal.com/developers/hastebin/{pastebin_data['key']})", ephemeral=True)
+        self.add_item(disnake.ui.Button(label="Остальные существующие валюты", url=f"https://www.toptal.com/developers/hastebin/hogaraqufe"))
