246e5d377ec4b382c2318b97872ceb4e401be178
MagMigo <magmigo2@inbox.ru>
2022-03-13 23:07:09 +0800

    Update utilities.py

--- cogs/slash_commands/utilities.py
+++ cogs/slash_commands/utilities.py
@@ -189,7 +189,7 @@ class Utilities(commands.Cog):
             await inter.send(
                 embed=await self.bot.embeds.simple(
                     title=f'Курс - {get_currency[upper_currency]} ({upper_currency})',
-                    description=f'{get_currency[upper_currency]} на данный момент стоит **{round(data[upper_currency]["Value"])} ({round(data[upper_currency]["Value"], 2)})** рублей. ({round(data[upper_currency]["Value"] - data[upper_currency]["Previous"], 1)})',
+                    description=f'Один {get_currency[upper_currency]} на данный момент стоит **{round(data[upper_currency]["Value"])} ({round(data[upper_currency]["Value"], 2)})** рублей. ({round(data[upper_currency]["Value"] - data[upper_currency]["Previous"], 1)})',
                     fields=[
                         {
                             "name": "Абсолютная погрешность", 
