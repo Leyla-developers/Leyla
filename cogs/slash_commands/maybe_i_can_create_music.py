7391d8c95d397bbe6c9a24481df223861e62431b
MagMigo <magmigo2@inbox.ru>
2022-03-15 19:45:11 +0800

    Update maybe_i_can_create_music.py

--- cogs/slash_commands/maybe_i_can_create_music.py
+++ cogs/slash_commands/maybe_i_can_create_music.py
@@ -4,13 +4,86 @@ from disnake.ext import commands
 from Tools.exceptions import CustomError
 
 
+class LavalinkVoiceClient(disnake.VoiceClient):
+    """
+    This is the preferred way to handle external voice sending
+    This client will be created via a cls in the connect method of the channel
+    see the following documentation:
+    https://disnakepy.readthedocs.io/en/latest/api.html#voiceprotocol
+    """
+
+    def __init__(self, client: disnake.Client, channel: disnake.abc.Connectable):
+        self.client = client
+        self.channel = channel
+        # ensure there exists a client already
+        if hasattr(self.client, 'lavalink'):
+            self.lavalink = self.client.lavalink
+        else:
+            self.client.lavalink = lavalink.Client(client.user.id)
+            self.client.lavalink.add_node(
+                    '0.0.0.0',
+                    7000,
+                    'test',
+                    'us',
+                    'default-node')
+            self.lavalink = self.client.lavalink
+
+    async def on_voice_server_update(self, data):
+        # the data needs to be transformed before being handed down to
+        # voice_update_handler
+        lavalink_data = {
+                't': 'VOICE_SERVER_UPDATE',
+                'd': data
+                }
+        await self.lavalink.voice_update_handler(lavalink_data)
+
+    async def on_voice_state_update(self, data):
+        # the data needs to be transformed before being handed down to
+        # voice_update_handler
+        lavalink_data = {
+                't': 'VOICE_STATE_UPDATE',
+                'd': data
+                }
+        await self.lavalink.voice_update_handler(lavalink_data)
+
+    async def connect(self, *, timeout: float, reconnect: bool) -> None:
+        """
+        Connect the bot to the voice channel and create a player_manager
+        if it doesn't exist yet.
+        """
+        # ensure there is a player_manager when creating a new voice_client
+        self.lavalink.player_manager.create(guild_id=self.channel.guild.id)
+        await self.channel.guild.change_voice_state(channel=self.channel)
+
+    async def disconnect(self, *, force: bool) -> None:
+        """
+        Handles the disconnect.
+        Cleans up running player and leaves the voice client.
+        """
+        player = self.lavalink.player_manager.get(self.channel.guild.id)
+
+        # no need to disconnect if we are not connected
+        if not force and not player.is_connected:
+            return
+
+        # None means disconnect
+        await self.channel.guild.change_voice_state(channel=None)
+
+        # update the channel_id of the player to None
+        # this must be done because the on_voice_state_update that
+        # would set channel_id to None doesn't get dispatched after the 
+        # disconnect
+        player.channel_id = None
+        self.cleanup()
+
 class Music(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
         
-        self.music = lavalink.Client(898664959767113729)
-        self.music.add_node('0.0.0.0', 7000, 'test', 'us', 'default-node')
+        if hasattr(bot, 'lavalink'):
+            self.music = lavalink.Client(898664959767113729)
+            self.music.add_node('0.0.0.0', 7000, 'test', 'us', 'default-node')
 
     @commands.command()
     async def join(self, ctx):
