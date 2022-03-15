9d66b9b76b881168dcc5b5cb7046caedced546a9
MagMigo <magmigo2@inbox.ru>
2022-03-15 19:29:16 +0800

    sadasd

---
+++ cogs/slash_commands/maybe_i_can_create_music.py
@@ -0,0 +1,23 @@
+import disnake
+import lavalink
+from disnake.ext import commands
+from Tools.exceptions import CustomError
+
+
+class Music(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+        
+        self.music = lavalink.Client(898664959767113729)
+        self.music.add_node('0.0.0.0', 2333, 'test', 'us', 'default-node')
+
+    @commands.command()
+    async def join(self, ctx):
+        if ctx.author.voice.channel:
+            await ctx.author.voice.channel.connect()
+        else:
+            raise CustomError("Ты забыл(-а) подключиться к голосовому каналу, Зайка!")
+
+def setup(bot):
+    bot.add_cog(Music(bot))
