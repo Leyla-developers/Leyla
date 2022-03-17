3a5ee6d607b7449236935ed0c541522dc3b7b19b
MagMigo <magmigo2@inbox.ru>
2022-03-17 19:19:33 +0800

    Сделал библиотеку под переводчик

--- Tools/translator.py
+++ Tools/translator.py
@@ -1,19 +1,9 @@
-import html
-import re
-
-import aiohttp
+from google.translator import GoogleTranslator
 
 
 class Translator:
 
-    def normal_data(self, text):
-        return html.unescape(text)
-
     async def translate(self, text, to_lang, from_lang):
-        async with aiohttp.ClientSession().get(f'https://translate.google.com/m?tl={to_lang}&sl={from_lang}&q={text}') as response:
-            data = await response.read()
+        google = GoogleTranslator()
 
-        data = data.decode('utf-8')
-        result = re.findall(r'(?s)class="(?:t0|result-container)">(.*?)<', data)
-
-        return self.normal_data(result[0])
+        return await google.translate(text, to_lang, from_lang)
