4c8a4937e2dd8450df63ceed54ef5373fcf95686
MagMigo <magmigo2@inbox.ru>
2022-03-20 12:44:39 +0800

    Update genshin.py

--- cogs/slash_commands/genshin.py
+++ cogs/slash_commands/genshin.py
@@ -31,56 +31,58 @@ class Genshin(commands.Cog):
         except:
             raise NotLoggedIn
 
-        statistics = self.gs.get_user_stats(uid)['stats']
-        fields = [
-            {
-                "name": "Количество персонажей",
-                "value": statistics["characters"],
-                "inline": True
-            },
-            {
-                "name": "Количество достижений",
-                "value": statistics['achievements'],
-                "inline": True
-            },
-            {
-                "name": "Дней активности",
-                "value": statistics['active_days'],
-                "inline": True
-            },
-            {
-                "name": "Витая бездна",
-                "value": statistics['spiral_abyss'],
-                "inline": True
-            },
-            {
-                "name": "Окулы",
-                "value": f"Анемокулы: {statistics['anemoculi']} | Геокулы: {statistics['geoculi']} | Электрокулы: {statistics['electroculi']}",
-                "inline": True,
-            },
-            {
-                "name": "Собрано сундуков",
-                "value": f"Обычных сундуков: {statistics['common_chests']} | Богатых сундуков: {statistics['exquisite_chests']}\nДрагоценных сундуков: {statistics['precious_chests']} | Роскошных сундуков: {statistics['luxurious_chests']}",
-                "inline": True,
-            },
-            {
-                "name": "Разблокировано точек телепортации",
-                "value": statistics['unlocked_waypoints'],
-                "inline": True
-            },
-            {
-                "name": "Разблокировано подземелий",
-                "value": statistics['unlocked_domains'],
-                "inline": True
-            }
-        ]
-        embed = await self.bot.embeds.simple(title=f'Информация о {uid}')
-        embed.description = 'Персонажи игрока (из профиля): ' + ', '.join(data['characters'])
+        else:
 
-        #for i in fields:
-        #    embed.add_field(name=i.get('name'), value=i.get('value'), inline=i.get('inline') if i.get('inline') else None)
+            statistics = self.gs.get_user_stats(uid)['stats']
+            fields = [
+                {
+                    "name": "Количество персонажей",
+                    "value": statistics["characters"],
+                    "inline": True
+                },
+                {
+                    "name": "Количество достижений",
+                    "value": statistics['achievements'],
+                    "inline": True
+                },
+                {
+                    "name": "Дней активности",
+                    "value": statistics['active_days'],
+                    "inline": True
+                },
+                {
+                    "name": "Витая бездна",
+                    "value": statistics['spiral_abyss'],
+                    "inline": True
+                },
+                {
+                    "name": "Окулы",
+                    "value": f"Анемокулы: {statistics['anemoculi']} | Геокулы: {statistics['geoculi']} | Электрокулы: {statistics['electroculi']}",
+                    "inline": True,
+                },
+                {
+                    "name": "Собрано сундуков",
+                    "value": f"Обычных сундуков: {statistics['common_chests']} | Богатых сундуков: {statistics['exquisite_chests']}\nДрагоценных сундуков: {statistics['precious_chests']} | Роскошных сундуков: {statistics['luxurious_chests']}",
+                    "inline": True,
+                },
+                {
+                    "name": "Разблокировано точек телепортации",
+                    "value": statistics['unlocked_waypoints'],
+                    "inline": True
+                },
+                {
+                    "name": "Разблокировано подземелий",
+                    "value": statistics['unlocked_domains'],
+                    "inline": True
+                }
+            ]
+            embed = await self.bot.embeds.simple(title=f'Информация о {uid}')
+            embed.description = 'Персонажи игрока (из профиля): ' + ', '.join(data['characters'])
 
-        await inter.send(embed=embed)
+            for i in fields:
+                embed.add_field(name=i.get('name'), value=i.get('value'), inline=i.get('inline') if i.get('inline') else None)
+
+            await inter.send(embed=embed)
 
 
 def setup(bot):
