def5a5964325cadda5bb3ef3944283821d240a05
MagMigo <magmigo2@inbox.ru>
2022-03-27 11:12:29 +0800

    Update images.py

--- Tools/images.py
+++ Tools/images.py
@@ -1,27 +1,15 @@
-# https://ru.stackoverflow.com/questions/581788/Как-создать-круглый-портрет-в-pil
-from PIL import Image, ImageDraw
-from io import BytesIO
+import easy_pil as epil
 
 
-import disnake
+def user_rank_card(member, lvl, xp, need_xp, percentage: int = 0):
+    font = epil.Font(path='data/assets/fonts/ubuntu.ttf', size=80)
+    small_font = epil.Font(path='data/assets/fonts/ubuntu.ttf', size=50)
+    back = epil.Editor('data/assets/images/card.png')
+    avatar = epil.Editor(epil.load_image(member.display_avatar.url)).circle_image().resize((450, 450))
 
-
-def prepare_mask(size, antialias = 2):
-    mask = Image.new('L', (size[0] * antialias, size[1] * antialias), 0)
-    ImageDraw.Draw(mask).ellipse((0, 0) + mask.size, fill=255)
-    return mask.resize(size, Image.ANTIALIAS)
-
-def crop(im, s):
-    w, h = im.size
-    k = w / s[0] - h / s[1]
-    if k > 0: im = im.crop(((w - h) / 2, 0, (w + h) / 2, h))
-    elif k < 0: im = im.crop((0, (h - w) / 2, w, (h + w) / 2))
-    return im.resize(s, Image.ANTIALIAS)
-
-def get_mask():
-    size = (420, 420)
-    im = Image.open('rounded_thumbnail.png')
-    im = crop(im, size)
-    im.putalpha(prepare_mask(size, 4))
-    im.save('rounded_thumbnail.png')
-    return 
+    back.paste(avatar, (0, 15))
+    back.bar((467, 342), max_width=967, height=66, fill='#419ff1', percentage=percentage)
+    back.text((470, 265), str(member), font=font, color="#00fa81")
+    back.text((855, 355), f"{xp} / {need_xp}", font=small_font, color="black")
+    back.text((1300, 280), str(lvl), font=font, color="#00fa81")
+    return back
