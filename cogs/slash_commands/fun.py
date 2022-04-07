64373a9d630552b13b926a635798231e92778bef
MagMigo <magmigo2@inbox.ru>
2022-04-07 19:27:22 +0800

    Update fun.py

--- cogs/slash_commands/fun.py
+++ cogs/slash_commands/fun.py
@@ -144,16 +144,13 @@ class FunSlashCommands(commands.Cog):
 
                                 if rand == 1:
                                     await message.channel.send(f'Тебе повезло :). Следующий: {member.mention}')
+                                elif len(data['joined']) == 1:
+                                    await message.channel.send(f"А {member.mention} везунчик.. Ты победил(-а)!")
+                                    await self.bot.config.DB.russian_roulette.delete_one({"_id": message.guild.id})
                                 else:
                                     await self.bot.config.DB.russian_roulette.update_one({"_id": message.guild.id}, {"$pull": {"joined": message.author.id, "step": message.author.id}})
                                     await self.bot.config.DB.russian_roulette.update_one({"_id": message.guild.id}, {"$set": {"step": [data['joined'][1 if len(data['joined']) > 1 else 0]]}})
-                                    if len(data['joined']) == 1:
-                                        await message.channel.send(f"А {member.mention} везунчик.. Ты победил(-а)!")
-                                        await self.bot.config.DB.russian_roulette.delete_one({"_id": message.guild.id})
-                                    else:
-                                        await self.bot.config.DB.russian_roulette.update_one({"_id": message.guild.id}, {"$pull": {"joined": message.author.id, "step": message.author.id}})
-                                        await self.bot.config.DB.russian_roulette.update_one({"_id": message.guild.id}, {"$set": {"step": [data['joined'][1 if len(data['joined']) > 1 else 0]]}})
-                                        await message.channel.send(f'Тебе не повезло, выбываешь. :(. Следующий: {member.mention}')
+                                    await message.channel.send(f'Тебе не повезло, выбываешь. :(. Следующий: {member.mention}')
 
                         if int(data['start_time']) == int(datetime.datetime.now().strftime('%H%M'))+5:
                             await message.channel.send('Игра окончена. Время выбыло')
