d665a168a9ad3a1743398024cdf6248cde7c8272
MagMigo <magmigo2@inbox.ru>
2022-04-07 19:31:11 +0800

    Update fun.py

--- cogs/slash_commands/fun.py
+++ cogs/slash_commands/fun.py
@@ -137,6 +137,7 @@ class FunSlashCommands(commands.Cog):
                             for i in range(1, len(data['joined'])):
                                 if len(data['joined']) > 1:
                                     await self.bot.config.DB.russian_roulette.update_one({"_id": message.guild.id}, {"$set": {"step": [data['joined'][i]]}})
+                                    await self.bot.config.DB.russian_roulette.update_one({"_id": message.guild.id}, {"$set": {"step": [data['joined'][1 if len(data['joined']) > 1 else 0]]}})
                                     member = message.guild.get_member(int(data['joined'][i]))
                                 else:
                                     await message.channel.send(f"А {member.mention} везунчик.. Ты победил(-а)!")
@@ -146,7 +147,6 @@ class FunSlashCommands(commands.Cog):
                                     await message.channel.send(f'Тебе повезло :). Следующий: {member.mention}')
                                 else:
                                     await self.bot.config.DB.russian_roulette.update_one({"_id": message.guild.id}, {"$pull": {"joined": message.author.id, "step": message.author.id}})
-                                    await self.bot.config.DB.russian_roulette.update_one({"_id": message.guild.id}, {"$set": {"step": [data['joined'][1 if len(data['joined']) > 1 else 0]]}})
                                     await message.channel.send(f'Тебе не повезло, выбываешь. :(. Следующий: {member.mention}')
 
                         if int(data['start_time']) == int(datetime.datetime.now().strftime('%H%M'))+5:
