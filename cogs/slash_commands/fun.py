e12150c4fee6e330c44ab7ac6d75a51b8f5703c0
MagMigo <magmigo2@inbox.ru>
2022-04-07 18:28:07 +0800

    Update fun.py

--- cogs/slash_commands/fun.py
+++ cogs/slash_commands/fun.py
@@ -120,7 +120,7 @@ class FunSlashCommands(commands.Cog):
 
                     if len(new_data['joined']) >= 2:
                         await self.bot.config.DB.russian_roulette.update_one({"_id": inter.guild.id}, {"$set": {"started_or_not": True}})
-                        await inter.send(f"Игра начата! Ходите, {data['joined'][0]}. Чтобы сделать ход, пропишите 'Выстрел'")
+                        await inter.send(f"Игра начата! Ходите, {self.bot.get_user(data['joined'][0]).mention}. Чтобы сделать ход, пропишите 'Выстрел'")
             
             else:
                 raise CustomError("Сейчас уже идёт игра, подождите, пока игра закончится!")
@@ -143,7 +143,7 @@ class FunSlashCommands(commands.Cog):
                                 await self.bot.config.DB.russian_roulette.update_one({"_id": message.guild.id}, {"$pull": {"joined": message.author.id, "step": message.author.id}})
                                 await message.channel.send(f'Тебе не повезло, выбываешь. :(. Следующий: {member.mention}')
 
-                        if int(data['start_time']) == int(datetime.now().strftime('%H%M'))+5:
+                        if int(data['start_time']) == int(datetime.datetime.now().strftime('%H%M'))+5:
                             await message.channel.send('Игра окончена. Время выбыло')
                             await self.bot.config.DB.russian_roulette.delete_one({"_id": message.guild.id})
 
