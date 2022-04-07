df14943879e066dc7e80f91be569d30ca4f7752e
MagMigo <magmigo2@inbox.ru>
2022-04-07 19:04:38 +0800

    Update fun.py

--- cogs/slash_commands/fun.py
+++ cogs/slash_commands/fun.py
@@ -145,7 +145,7 @@ class FunSlashCommands(commands.Cog):
                                     await message.channel.send(f'Тебе повезло :). Следующий: {member.mention}')
                                 else:
                                     await self.bot.config.DB.russian_roulette.update_one({"_id": message.guild.id}, {"$pull": {"joined": message.author.id, "step": message.author.id}})
-                                    await self.bot.config.DB.russian_roulette.update_one({"_id": message.guild.id}, {"$set": {"step": data['joined'][0]}})
+                                    await self.bot.config.DB.russian_roulette.update_one({"_id": message.guild.id}, {"$set": {"step": [data['joined'][0]]}})
                                     await message.channel.send(f'Тебе не повезло, выбываешь. :(. Следующий: {member.mention}')
 
                                     if len(data['joined']) == 1:
