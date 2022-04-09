6f72df192d954d158070f40d52fcdf363b4aeca9
MagMigo <magmigo2@inbox.ru>
2022-04-09 18:01:15 +0800

    Update marries.py

--- cogs/slash_commands/marries.py
+++ cogs/slash_commands/marries.py
@@ -19,22 +19,29 @@ class MarryButton(disnake.ui.View):
         if self.partner.id != inter.author.id:
             await inter.response.send_message("Принять должен тот, кого вы попросили!", ephemeral=True)
         else:
-            msg = await inter.response.send_message(f'{self.partner.mention} Согласен(на) быть партнёром {inter.author.mention} 🎉')
-            await msg.edit(view=None)
+            await inter.response.send_message(f'{self.partner.mention} Согласен(на) быть партнёром {inter.author.mention} 🎉')
             await self.config.DB.marries.insert_one({"_id": inter.author.id, "mate": self.partner.id, 'time': datetime.now()})
+            self.stop()
 
     @disnake.ui.button(label="Отказать", style=disnake.ButtonStyle.red)
     async def marry_button_cancel(self, button, inter):
         if self.partner.id != inter.author.id:
             await inter.response.send_message("Нажать должен(на) тот, кого вы попросили!", ephemeral=True)
         else:
-            msg = await inter.response.send_message(f'{self.partner.mention} Не согласен(на) быть партнёром {inter.author.mention}')
-            await msg.edit(view=None)
+            await inter.response.send_message(f'{self.partner.mention} Не согласен(на) быть партнёром {inter.author.mention}')
+            self.stop()
 
 class Marries(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
+
+    async def is_married(self, author, member):
+        if await self.bot.config.DB.marries.count_documents({"_id": author.id}) == 0 or await self.bot.config.DB.marries.count_documents({"mate": member.id}) == 0 or \
+            await self.bot.config.DB.marries.count_documents({"_id": member.id}) == 0 or await self.bot.config.DB.marries.count_documents({"mate": author.id}) == 0:
+            return True
+        else:
+            return False
 
     @commands.slash_command(name='marry', description="Свадьбы")
     async def marry_cmd(self, inter):
@@ -42,19 +49,15 @@ class Marries(commands.Cog):
 
     @marry_cmd.sub_command(name="invite", description="Предложить сыграть свадьбу кому-либо")
     async def marry_invite(self, inter, member: disnake.Member):
-        if await self.bot.config.DB.marries.count_documents({"_id": inter.author.id}) == 0 or await self.bot.config.DB.marries.count_documents({"_id": member.id}) == 0 or \
-            await self.bot.config.DB.marries.count_documents({"mate": inter.author.id}) == 0 or await self.bot.config.DB.marries.count_documents({"mate": member.id}) == 0:
+        if await self.is_married(inter.author, member):
             view = MarryButton(partner=member)
-            main_description = f"{inter.author.mention} предлагает {member.mention} сыграть свадьбу. Ммм...)"
-            embed = await self.bot.embeds.simple(
-                        title="Свадьба, получается <3", 
-                        description=main_description,
-                        footer={"text": "Только, давайте, без беременная в 16, хорошо?", 'icon_url': inter.author.display_avatar.url}
-                    )
-            msg = await inter.send(embed=embed, view=view)
-
-            if view.disabled:
-                await msg.edit(view=None)
+            await inter.send(
+                embed=await self.bot.embeds.simple(
+                    title="Свадьба, получается <3", 
+                    description=f"{inter.author.mention} предлагает {member.mention} сыграть свадьбу. Ммм...)",
+                    footer={"text": "Только, давайте, без беременная в 16, хорошо?", 'icon_url': inter.author.display_avatar.url}
+                ), view=view
+            )
         else:
             raise CustomError(f"Эм) Вы и/или {member.mention} женаты. На что вы надеетесь?")
 
