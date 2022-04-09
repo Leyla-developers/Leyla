e6cad713882c7705c71ff31e82fd381e1eedc719
MagMigo <magmigo2@inbox.ru>
2022-04-09 16:58:21 +0800

    Update marries.py

--- cogs/slash_commands/marries.py
+++ cogs/slash_commands/marries.py
@@ -7,6 +7,7 @@ from Tools.exceptions import CustomError
 
 
 class MarryButton(disnake.ui.View):
+    message: disnake.Message
 
     def __init__(self, partner: disnake.Member):
         super().__init__(timeout=15)
@@ -21,30 +22,30 @@ class MarryButton(disnake.ui.View):
 
     @disnake.ui.button(label="Принять", style=disnake.ButtonStyle.green)
     async def marry_button_accept(self, button, inter):
-        #if inter.author.id == self.partner.id:
-        #    await inter.response.send_message("Принять должен тот, кого вы попросили!", ephemeral=True)
-        #else:
-        for child in self.children:
-            if isinstance(child, disnake.ui.Button):
-                child.disabled = True
+        if inter.author.id == self.partner.id:
+            await inter.response.send_message("Принять должен тот, кого вы попросили!", ephemeral=True)
+        else:
+            for child in self.children:
+                if isinstance(child, disnake.ui.Button):
+                    child.disabled = True
 
-        await inter.response.send_message(f'{inter.author.mention} Согласен(на) быть партнёром {self.partner.mention}')
-        await self.config.DB.marries.insert_one({"_id": inter.author.id, "mate": self.partner.id, 'time': datetime.now()})
-        self.value = True
-        self.stop()
+            await inter.response.send_message(f'{self.partner.mention} Согласен(на) быть партнёром {inter.author.mention} 🎉')
+            await self.config.DB.marries.insert_one({"_id": inter.author.id, "mate": self.partner.id, 'time': datetime.now()})
+            self.value = True
+            self.stop()
 
     @disnake.ui.button(label="Отказать", style=disnake.ButtonStyle.red)
     async def marry_button_cancel(self, button, inter):
-        #if inter.author.id == self.partner.id:
-        #    await inter.response.send_message("Нажать должен(на) тот, кого вы попросили!", ephemeral=True)
-        #else:
-        for child in self.children:
-            if isinstance(child, disnake.ui.Button):
-                child.disabled = True
-                
-        await inter.response.send_message(f'{inter.author.mention} Не согласен(на) быть партнёром {self.partner.mention}')
-        self.value = False
-        self.stop()
+        if inter.author.id == self.partner.id:
+            await inter.response.send_message("Нажать должен(на) тот, кого вы попросили!", ephemeral=True)
+        else:
+            for child in self.children:
+                if isinstance(child, disnake.ui.Button):
+                    child.disabled = True
+                    
+            await inter.response.send_message(f'{self.partner.mention} Не согласен(на) быть партнёром {inter.author.mention} 🎉')
+            self.value = False
+            self.stop()
 
 class Marries(commands.Cog):
 
@@ -59,24 +60,14 @@ class Marries(commands.Cog):
     async def marry_invite(self, inter, member: disnake.Member):
         if await self.bot.config.DB.marries.count_documents({"_id": inter.author.id}) == 0 or await self.bot.config.DB.marries.count_documents({"_id": member.id}) == 0 or \
         await self.bot.config.DB.marries.count_documents({"mate": inter.author.id}) == 0 or await self.bot.config.DB.marries.count_documents({"mate": member.id}) == 0:
-            view = MarryButton(partner=inter.author)
+            view = MarryButton(partner=member)
             main_description = f"{inter.author.mention} предлагает {member.mention} сыграть свадьбу. Ммм...)"
             embed = await self.bot.embeds.simple(
                         title="Свадьба, получается <3", 
                         description=main_description,
                         footer={"text": "Только, давайте, без беременная в 16, хорошо?", 'icon_url': inter.author.display_avatar.url}
                     )
-            message = await inter.send(embed=embed, view=view)
-            await view.wait()
-
-            if view.value:
-                embed.description = f"Свадьбе быть! {inter.author.mention} связал(а) свои брачные узы с {member.mention} :3"
-                await message.edit(embed=embed)
-            elif view.disabled:
-                await message.edit('Всё, всё. Что вы пытаетесь ещё-то?')
-            else:
-                embed.description = f"Свадьбе не быть(. {member.mention} отказал(а) {inter.author.mention}"
-                await message.edit(embed=embed)
+            await inter.send(embed=embed, view=view)
         else:
             raise CustomError(f"Эм) Вы и/или {member.mention} женаты. На что вы надеетесь?")
 
