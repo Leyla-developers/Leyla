61fe3404947857f3ed0a174f063f9068c9b31a6c
MagMigo <magmigo2@inbox.ru>
2022-04-16 17:58:47 +0800

    Update core_events.py

--- cogs/events/core_events.py
+++ cogs/events/core_events.py
@@ -22,7 +22,22 @@ class CoreEvents(commands.Cog):
                 fields=[
                     {"name": "Участников", "value": len(guild.member)},
                     {"name": "Ботов", "value": len([i.id for i in guild.members if i.bot])}
-                ]
+                ],
+                color=disnake.Color.green()
+            )
+        )
+
+    @commands.Cog.listener()
+    async def on_guild_remove(self, guild):
+        channel = self.bot.get_channel(864408447029215232)
+        await channel.send(
+            embed=await self.bot.embeds.simple(
+                title=f'Меня убрали с {guild.name}', 
+                fields=[
+                    {"name": "Участников", "value": len(guild.member), "inline": True},
+                    {"name": "Ботов", "value": len([i.id for i in guild.members if i.bot]), "inline": True}
+                ],
+                color=disnake.Color.red()
             )
         )
 
