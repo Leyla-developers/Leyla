933938f3e2cde0c81d78abed2be84d40821a526b
MagMigo <magmigo2@inbox.ru>
2022-04-16 17:37:42 +0800

    Update core_events.py

--- cogs/events/core_events.py
+++ cogs/events/core_events.py
@@ -13,6 +13,9 @@ class CoreEvents(commands.Cog):
         if message.content == self.bot.user.mention:
             await message.reply('Да, да, что такое? Я здесь, Старшина Сенпай!\nКоманды ты можешь посмотреть, введя `/` и найди мою аватарку в списке ботов. Там будут все команды, которые я могу тебе дать')
     
-
+    @commands.Cog.listener()
+    async def on_command(self, ctx):
+        await ctx.reply(f'{ctx.author.name}-Сан! Более обычные команды, начинающиеся с {ctx.prefix}, больше работать не будут. Теперь используйте слэш-команды `/`')
+    
 def setup(bot):
     bot.add_cog(CoreEvents(bot))
