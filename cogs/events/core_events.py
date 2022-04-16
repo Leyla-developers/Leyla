b008502155e18f37c65f33a12781d6a800c0ec32
MagMigo <magmigo2@inbox.ru>
2022-04-16 17:54:58 +0800

    Update core_events.py

--- cogs/events/core_events.py
+++ cogs/events/core_events.py
@@ -13,5 +13,18 @@ class CoreEvents(commands.Cog):
         if message.content == self.bot.user.mention:
             await message.reply('Да, да, что такое? Я здесь, Старшина Сенпай!\nКоманды ты можешь посмотреть, введя `/` и найди мою аватарку в списке ботов. Там будут все команды, которые я могу тебе дать')
 
+    @commands.Cog.listener()
+    async def on_guild_join(self, guild):
+        channel = self.bot.get_channel(864408447029215232)
+        await channel.send(
+            embed=await self.bot.embeds.simple(
+                title=f'Меня добавили на {guild.name}', 
+                fields=[
+                    {"name": "Участников", "value": len(guild.member)},
+                    {"name": "Ботов", "value": len([i.id for i in guild.members if i.bot])}
+                ]
+            )
+        )
+
 def setup(bot):
     bot.add_cog(CoreEvents(bot))
