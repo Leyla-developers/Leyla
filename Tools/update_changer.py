2c1156d53ba7741c0d4b8e31e00d8e6cf3618a61
Nukchix <yellowrussiayt@gmail.com>
2023-06-28 12:54:14 +0300

    fix update_changer

--- Tools/update_changer.py
+++ Tools/update_changer.py
@@ -2,7 +2,7 @@ import disnake
 from disnake.ext import commands
 
 
-def updated_username(user: disnake.User):
+def updated_username(user: disnake.User or disnake.Member):
     if user.discriminator == "0":
         return user.name
     else:
