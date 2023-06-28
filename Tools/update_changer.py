2fae455df92680e9ab8c76b86d328f154fac134a
Nukchix <yellowrussiayt@gmail.com>
2023-06-28 15:15:56 +0300

    Update update_changer.py

--- Tools/update_changer.py
+++ Tools/update_changer.py
@@ -5,5 +5,4 @@ from disnake.ext import commands
 def updated_username(user: disnake.User or disnake.Member):
     if user.discriminator == "0":
         return user.name
-    else:
-        return str(user)
+    else: return str(user)
